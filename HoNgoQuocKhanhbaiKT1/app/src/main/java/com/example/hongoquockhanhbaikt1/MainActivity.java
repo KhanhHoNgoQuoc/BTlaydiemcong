740d5c0f9be064c98002a852806902b95b6e56d9
khanh <khanhho356566@gmail.com>
2022-04-11 18:26:00 +0700

    first commit

---
+++ HoNgoQuocKhanhbaiKT1/app/src/main/java/com/example/hongoquockhanhbaikt1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.hongoquockhanhbaikt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button nut = (Button) findViewById(R.id.btnlogin);
+        nut.setOnClickListener(thongbao);
+    }
+    View.OnClickListener thongbao = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText tk = (EditText)findViewById(R.id.edtuser);
+            EditText mk = (EditText)findViewById(R.id.edtpass);
+            String user = "maicuongtho";
+            String pass = "Cntt60ntu!";
+            String t = tk.getText().toString();
+            String m = mk.getText().toString();
+            if ((t.equals(user)) && (m.equals(pass))){
+                Toast.makeText(MainActivity.this, "User and Password is correct", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "User and Password is wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
